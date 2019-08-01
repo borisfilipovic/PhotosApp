338647fcb167b2f373420abc376acb2dff4cf225
Boris Filipovic <boris.filipovic1@gmail.com>
2019-08-01 20:48:53 +0200

    Initial project setup.

---
+++ PhotosApp/PhotosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Boris Filipovic ENGL on 01/08/2019.
+//  Copyright © 2019 Boris Filipovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
